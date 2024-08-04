42e1c529b6fe5e25430a8127b84e2e3f5ec0b010
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-08-04 18:10:07 +0500

    Add files via upload

---
+++ news/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-post_time'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
